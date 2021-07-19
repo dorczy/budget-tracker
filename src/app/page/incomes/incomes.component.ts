645f074942ff798877854e334357c11d9d4cf7ae
Kovács Dorottya <kovacsdorika@gmail.com>
2021-07-20 00:32:28 +0200

    IncomesComponent.

---
+++ src/app/page/incomes/incomes.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { TableColumn } from 'src/app/interface/table-column';
+import { Income } from 'src/app/model/income';
+import { IncomeService } from 'src/app/service/income.service';
+
+@Component({
+  selector: 'app-incomes',
+  templateUrl: './incomes.component.html',
+  styleUrls: ['./incomes.component.scss']
+})
+export class IncomesComponent implements OnInit {
+
+  data$: BehaviorSubject<Income[]> = this.incomeService.list$;
+
+  config: TableColumn[] = this.incomeService.config;
+  itemIcon: string = this.incomeService.itemIcon;
+  routerName: string = this.incomeService.routerName;
+
+  constructor(
+    private incomeService: IncomeService,
+  ) {
+    this.incomeService.getAll();
+  }
+
+  ngOnInit(): void {
+  }
+
+}
