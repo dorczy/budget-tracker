f2a5851974595ede6e8fdfd5123873ecf6e372ea
Kovács Dorottya <kovacsdorika@gmail.com>
2021-07-31 21:42:43 +0200

    ListSummaryComponent.

---
+++ src/app/page/list-summary/list-summary.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { TableColumn } from 'src/app/interface/table-column';
+import { Income } from 'src/app/model/income';
+import { IncomeService } from 'src/app/service/income.service';
+import { Expense } from 'src/app/model/expense';
+import { ExpenseService } from 'src/app/service/expense.service';
+
+@Component({
+  selector: 'app-list-summary',
+  templateUrl: './list-summary.component.html',
+  styleUrls: ['./list-summary.component.scss']
+})
+export class ListSummaryComponent implements OnInit {
+
+  incomes$: BehaviorSubject<Income[]> = this.incomeService.list$;
+  expenses$: BehaviorSubject<Expense[]> = this.expenseService.list$;
+
+  allDatas: any[] = [];
+
+  incomeConfig: TableColumn[] = this.incomeService.config;
+  incomeItemIcon: string = this.incomeService.itemIcon;
+  incomeRouterName: string = this.incomeService.routerName;
+
+  expenseConfig: TableColumn[] = this.expenseService.config;
+  expenseItemIcon: string = this.expenseService.itemIcon;
+  expenseRouterName: string = this.expenseService.routerName;
+
+  // sort
+  ascend: boolean = false;
+  sortKey: string = '';
+
+  // filter
+  filterKey: string = '';
+  phrase: string = '';
+
+
+  constructor(
+    public incomeService: IncomeService,
+    public expenseService: ExpenseService,
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.incomes$.forEach(
+      incomes => this.allDatas = this.allDatas.concat(incomes)
+    );
+    this.expenses$.forEach(
+      expenses => this.allDatas = this.allDatas.concat(expenses)
+    );
+  }
+
+}
